1c1412fee660fa6e646dcc752e4b5b610f40492e
Kavya Gupta <95200924+Kavya2719@users.noreply.github.com>
2022-07-28 22:19:50 +0530

    Create DAY11_CC_N1SERIES.cpp

---
+++ DAY11_CC_N1SERIES.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define iofast() ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#define ff first
+
+#define ss second
+
+#define all(v) v.begin(),v.end()
+
+#define yes cout << "YES\n"
+
+#define no cout << "NO\n"
+
+#define ccase(t) "Case #" << t << ": "
+
+#define lp(i,x,sz) for(int i=x;i<sz;i++)
+
+#define ll long long
+
+const int N = (int)1e5 + 10;
+
+const int inf = (int)1e9+10;
+
+const int M = (int)1e9 + 7;
+
+void RadheRadhe(int t,bool kavya2719 = 1){
+
+   int sz;
+
+   cin >> sz;
+
+   
+
+   cout << "1 1 ";
+
+   for(ll i=2;i<(1ll<<sz);i*=2){
+
+     cout << i << " ";
+
+   }
+
+   cout << "\n";
+
+}
+
+void phle_ka_kaam(){
+
+   
+
+}
+
+int main()
+
+{
+
+    iofast();
+
+    phle_ka_kaam();
+
+    
+
+    int TTTT=1;
+
+    cin >> TTTT;
+
+    for(int i=1;i<=TTTT;i++)
+
+       RadheRadhe(i);
+
+}
